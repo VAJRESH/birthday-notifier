b8078f0c3969b24b3c3eef8deebd40a462b8a8bc
VAJRESH <vajresh005@gmail.com>
2021-07-11 01:08:29 +0530

    Update controllers.js

--- helper/controllers.js
+++ helper/controllers.js
@@ -9,11 +9,11 @@ exports.isBirthdayToday = (dateArray) => {
   const dob = new Date(year, month, date);
   const todayDate = new Date();
 
-  const birthYear = dob.getFullYear();
+  //const birthYear = dob.getFullYear();
   const birthMonth = dob.getMonth();
   const birthDate = dob.getDate();
 
-  const currentYear = todayDate.getFullYear();
+  //const currentYear = todayDate.getFullYear();
   const currentMonth = todayDate.getMonth();
   const currentDate = todayDate.getDate();
 
